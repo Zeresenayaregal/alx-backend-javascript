715722577db18156adce90c57743baf5f26388d1
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-10-08 23:37:39 +0300

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Mikiyas Dawit <https://github.com/MAYSHLMAY>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
